334e08feaed418e32b382a59254413b2d7168b9d
Ben Doherty <ben@notionparallax.co.uk>
2023-06-23 02:52:55 +0000

    add a ranger

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -1,9 +1,6 @@
-"""Your very first python program!
-
+"""Your very first python program! 👩‍🦰
 TODO: write a python script that prints "Hello world!"
-
 Look on the course website for how to do this, and for how to run the tests.
-
 https://design-computing.github.io/md/week1
 
 """
