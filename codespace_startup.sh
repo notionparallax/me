2b6d6c223da9ccf1eb3f2d6a1241d965a5bb07b8
Ben Doherty <ben@notionparallax.co.uk>
2023-06-23 02:50:55 +0000

    do week 1

--- codespace_startup.sh
+++ codespace_startup.sh
@@ -7,11 +7,11 @@ pip install -r ../course/requirements.txt
 bash ../course/extensions.sh
 python ../course/set1/tests.py
 
-# Before you run this file, change these to be your name and email address, 
-# then take the # off the value from just before the quote. 
+# Before you run this file, change these to be your name and email address,
+# then take the # off the value from just before the quote.
 # I.e. make #" int just "
-git config --global user.email #"email@example.com"
-git config --global user.name #"Ben Doherty"
+git config --global user.email "ben@notionparallax.co.uk"
+git config --global user.name "Ben Doherty"
 
 # once you've done this, run this file by copying this command to the terminal:
-# bash codespace_startup.sh 
+# bash codespace_startup.sh
